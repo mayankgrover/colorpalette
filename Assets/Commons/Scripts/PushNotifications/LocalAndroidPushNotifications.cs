fb299cfe2a9db7ddd9ca09ce3c15aa52ba975637
mayankgrover <mayankkgrover@gmail.com>
2017-05-13 20:52:01 +0530

    android local notification plugin is working, just the plugin!

---
+++ Assets/Commons/Scripts/PushNotifications/LocalAndroidPushNotifications.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+
+namespace Commons.PushNotifications
+{
+    public class LocalAndroidPushNotifications
+    {
+        public enum NotificationExecuteMode
+        {
+            Inexact = 0,
+            Exact = 1,
+            ExactAndAllowWhileIdle = 2
+        }
+
+        private static string fullClassName = "com.unforgivinggames.androidnotifications.LocalPushNotification";
+        private static string mainActivityClassName = "com.unity3d.player.UnityPlayerActivity";
+
+        public static void SendNotification(int id, TimeSpan delay, string title, string message)
+        {
+            SendNotification(id, (int)delay.TotalSeconds, title, message, Color.white);
+        }
+
+        public static void SendNotification(int id, long delay, string title, string message, Color32 bgColor, 
+            bool sound = true, bool vibrate = true, bool lights = true, string bigIcon = "notification_icon_big", 
+            string smallIcon = "notification_icon_small",
+            NotificationExecuteMode executeMode = NotificationExecuteMode.Inexact)
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            AndroidJavaClass pluginClass = new AndroidJavaClass(fullClassName);
+            if (pluginClass != null)
+            {
+                pluginClass.CallStatic("SetNotification", id, delay * 1000L, title, message, 
+                    message, sound ? 1 : 0, vibrate ? 1 : 0, lights ? 1 : 0, bigIcon, smallIcon, 
+                    bgColor.r * 65536 + bgColor.g * 256 + bgColor.b, (int)executeMode, mainActivityClassName);
+            }
+#endif
+        }
+
+        public static void SendRepeatingNotification(int id, long delay, long timeout, string title, 
+            string message, Color32 bgColor, bool sound = true, bool vibrate = true, 
+            bool lights = true, string bigIcon = "notification_icon_big", string smallIcon = "notification_icon_small")
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            AndroidJavaClass pluginClass = new AndroidJavaClass(fullClassName);
+            if (pluginClass != null)
+            {
+                pluginClass.CallStatic("SetRepeatingNotification", id, delay * 1000L, title, 
+                    message, message, timeout * 1000, sound ? 1 : 0, vibrate ? 1 : 0, lights ? 1 : 0, 
+                    bigIcon, smallIcon, bgColor.r * 65536 + bgColor.g * 256 + bgColor.b, 
+                    mainActivityClassName);
+            }
+#endif
+        }
+
+        public static void CancelNotification(int id)
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            AndroidJavaClass pluginClass = new AndroidJavaClass(fullClassName);
+            if (pluginClass != null)
+            {
+                pluginClass.CallStatic("CancelNotification", id);
+            }
+#endif
+        }
+
+        public static void CancelAllNotifications()
+        {
+#if UNITY_ANDROID && !UNITY_EDITOR
+            AndroidJavaClass pluginClass = new AndroidJavaClass(fullClassName);
+            if (pluginClass != null)
+            {
+                pluginClass.CallStatic("CancelAll");
+            }
+#endif
+        }
+    }
+}
---
+++ Assets/Commons/Scripts/PushNotifications/ServiceLocalPushNotifications.cs
@@ -0,0 +1,50 @@
+﻿using Commons.Singleton;
+using System;
+using UnityEngine;
+
+namespace Commons.PushNotifications
+{
+    public class ServiceLocalPushNotifications: MonoSingleton<ServiceLocalPushNotifications>
+    {
+        protected override void Start()
+        {
+            base.Start();
+            //TestLocalPushNotifications();
+        }
+
+        public void CancelNotification3()
+        {
+            LocalAndroidPushNotifications.CancelNotification(1003);
+            Debug.Log("CancelNotification: 3");
+        }
+
+        public void FireNotification1()
+        {
+            LocalAndroidPushNotifications.SendNotification(1001, TimeSpan.FromSeconds(5), "(1) Notification",
+                "Notification should fire in 10seconds from now");
+            Debug.Log("SendNotification: 1");
+        }
+
+        public void FireNotification2()
+        {
+            LocalAndroidPushNotifications.SendNotification(1002, 5, "(2) Notification",
+                "Notification should fire in 15seconds....", Color.black, bigIcon: "notification_icon_big");
+            Debug.Log("SendNotification: 2");
+        }
+
+        public void FireNotification3()
+        {
+            LocalAndroidPushNotifications.SendNotification(1003, 5, "(3) Notification",
+                "Notification should fire in 20seconds....", Color.white, bigIcon: "notification_icon_big");
+            Debug.Log("SendNotification: 3");
+
+        }
+
+        public void FireNotification4()
+        {
+            LocalAndroidPushNotifications.SendRepeatingNotification(1004, 5, 3, "(4) Repeat Notification",
+                "Notification should repeat every 8seconds..", Color.grey, smallIcon: "notification_icon_small");
+            Debug.Log("SendNotification: 4");
+        }
+    }
+}
